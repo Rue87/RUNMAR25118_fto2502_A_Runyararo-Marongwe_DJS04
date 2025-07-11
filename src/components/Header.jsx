5f15b7a1a5e689966e7f09340723c297803f2089
Rue87 <mrunya87@gmail.com>
2025-07-11 12:39:55 +0200

    feat: add icons to header section and apply corresponding CSS styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
+import searchicon from '../assets/searchIcon.png'; 
+import profileicon from '../assets/profileIcon.png'; 
+import '../index.css';
+/**
+ * Header component with logo, search icon, and user profile.
+ * @returns {JSX.Element}
+ */
 export default function Header() {
+
+    const handleSearchClick = () => {
+    const input = document.querySelector('input[type="text"]');
+    if (input) input.focus();
+  };
+
   return (
     <header className="app-header">
       <h1>🎙️ Podcast App</h1>
+
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1.5rem' }}>
+        <img
+          src={searchicon}
+          alt="Search"
+          className="search-icon"
+          onClick={handleSearchClick}
+        />
+        <img 
+          src={profileicon} 
+          alt="User Profile" 
+          className="profile-avatar" 
+        />
+      </div>
     </header>
   );
 }
